2f8e6af7333f3c8406020c28e1919c5692d63f44
viacheslav seledkin <vseledkin@ws24364.avicomp.com>
2017-09-18 17:09:08 +0300

    plda

--- bin/pbplda.go
+++ bin/pbplda.go
@@ -3,18 +3,207 @@ package main
 import (
 	"flag"
 	"fmt"
-	"runtime"
-	"time"
-	"unsafe"
+	"log"
+	"os"
+
+	"bufio"
+
+	".."
 )
 
+const (
+	model = "model"
+)
+
+var input, cwd string
+var topics int // number of topics
+
 func main() {
-	v := make([]float32, 100)
-	fmt.Println("[]float32 align ", unsafe.Alignof(v))
-	v64 := make([]float64, 100)
-	fmt.Println("[]float64 align ", unsafe.Alignof(v64))
-	runtime.GOMAXPROCS(runtime.NumCPU())
-	fmt.Println("CPU:", runtime.NumCPU())
+	modelCommand := flag.NewFlagSet(model, flag.ExitOnError)
+	modelCommand.StringVar(&input, "input", "", "text file one document per line")
+	modelCommand.IntVar(&topics, "t", 2, "number of topics")
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "%s <command> arguments\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "commands are:\n")
+
+		fmt.Fprintf(os.Stderr, "%s\n", model)
+		modelCommand.PrintDefaults()
+
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+	log.SetOutput(os.Stderr)
+
+	if len(os.Args) == 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	cwd, _ = os.Getwd()
+	log.Printf("Starting in %s directory.", cwd)
+	switch os.Args[1] {
+	case model:
+		modelCommand.Parse(os.Args[2:])
+	default:
+		log.Printf("%q is not valid command.\n", os.Args[1])
+		os.Exit(1)
+	}
+
+	// MODEL COMMAND ISSUED
+	if modelCommand.Parsed() {
+		if input == "" {
+			modelCommand.PrintDefaults()
+			return
+		}
+		if topics < 2 {
+			modelCommand.PrintDefaults()
+		}
+		err := Model(topics, input)
+		if err != nil {
+			panic(err)
+		}
+		return
+	}
+}
+
+func tf(t string) map[string]int {
+	m := make(map[string]int)
+	for _, token := range golda.Split(t) {
+		m[token]++
+	}
+	return m
+}
+
+func Model(t int, input string) error {
+	dic, err := golda.DictionaryFromFile(input)
+	if err != nil {
+		return err
+	}
+	i := 0
+	for i < 10 {
+		t := dic.TokenByID(i)
+		log.Printf("%d %s %d\n", t.ID, t.Token, t.Frequency)
+		i++
+	}
+	log.Printf("Dictionary has %d tokens\n", dic.Len())
+
+	// count number of nonzeros
+	nnz := 0
+	documents := 0
+	f, err := os.Open(input)
+	if err != nil {
+		return err
+	}
+	r := bufio.NewReader(f)
+	for {
+		line, e := r.ReadString('\n')
+		if e != nil {
+			break
+		}
+
+		if len(line) > 0 {
+			for token, count := range tf(line) {
+				if _, ok := dic.Tokens[token]; ok {
+					nnz += count
+				}
+			}
+			documents++
+		}
+	}
+	m := golda.NewSparseMatrix(nnz, dic.Len(), documents)
+	f.Seek(0, 0)
+	documents = 0
+	for {
+		line, e := r.ReadString('\n')
+		if e != nil {
+			break
+		}
+
+		if len(line) > 0 {
+			for token, count := range tf(line) {
+				if t, ok := dic.Tokens[token]; ok {
+					m.Set(t.ID, documents, float32(count))
+				}
+			}
+			documents++
+		}
+	}
+	f.Close()
+	m.Pack()
+	// make sparce matrix
+	log.Printf("Corpus has %d documents\n", documents)
+	log.Printf("Matrix has %d rows\n", m.Rows)
+	log.Printf("Matrix has %d columns\n", m.Cols)
+	log.Printf("Matrix has %d nonzeros\n", m.Nonzeros)
+
+	sparsity := 100 * float64(nnz) / (float64(m.Rows) * float64(m.Cols))
+	fmt.Printf("Matrix sparsity: %3.3f %%\n", sparsity)
+
+	var (
+		ALPHA     float32 = 0.01
+		BETA      float32 = 0.1
+		OUTPUT            = true
+		startcond         = false
+	)
+	mu := make([]float32, topics*int(m.Nonzeros))
+	phi := make([]float32, topics*int(m.Rows))
+	theta := make([]float32, topics*int(m.Cols))
+	m.AsyncronousBeliefPropagation(ALPHA, BETA, topics, 10000, startcond, OUTPUT, phi, theta, mu)
+	PrintTopics(dic, phi, topics, m.Rows, 10)
+	return nil
+}
+
+/*
+WeightedUint32 unsigned weighted number
+*/
+type WeightedInt struct {
+	ID     int
+	Weight float32
+}
+
+/*
+PrintTopics -prints top N words of every topic
+*/
+func PrintTopics(d *golda.Dictionary, phi []float32, topics, words, n int) {
+	var weight float32
+	for t := 0; t < topics; t++ {
+		fmt.Printf("Topic %d:\n", t)
+		items := make([]WeightedInt, n)
+		for i := 0; i < words; i++ {
+			weight = phi[i*topics+t]
+			for j := 0; j < n; j++ {
+				if weight > items[j].Weight {
+					for p := n - 1; p > j; p-- {
+						items[p].Weight = items[p-1].Weight
+					}
+					items[j].ID = i
+					items[j].Weight = weight
+					break
+				}
+			}
+		}
+		for i := 0; i < n; i++ {
+			fmt.Printf("\t%d %d %f %s\n", i, items[i].ID, items[i].Weight, d.TokenByID(items[i].ID).Token)
+		}
+	}
+}
+
+/*
+func maisn() {
+	corpus, dictionary, err := golda.LoadCorpus(input)
+	if err != nil {
+		return err
+	}
+	i := 0
+	for i < 10 {
+		t := dictionary.TokenByID(i)
+		log.Printf("%d %s %d\n", t.ID, t.Token, t.Frequency)
+		i++
+	}
+	log.Printf("Dictionary has %d tokens\n", dictionary.Len())
+	log.Printf("Corpus has %d documents\n", corpus.Len())
 
 	var in, out, voc string
 	var iterations, topics, threads, alignment int
@@ -68,5 +257,5 @@ func main() {
 	ai.PrintTopics(dictionary, phi, uint32(topics), m.Rows, 10)
 	fmt.Printf("Printiing finished for %v\n", time.Now().Sub(start))
 	fmt.Printf("PBPLDA finished for %v\n", pldadur)
-
 }
+*/
--- smatrix.go
+++ smatrix.go
@@ -1,21 +1,28 @@
 package golda
 
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"time"
+)
+
 /*
 SparseMatrix compressed sparse matrix
 */
 type SparseMatrix struct {
-	index, Nonzeros, Rows, Cols uint32
-	Ir, Jc                      []uint32
+	index, Nonzeros, Rows, Cols int
+	Ir, Jc                      []int
 	Sr                          []float32
 }
 
 /*
 NewSparseMatrix allocates new matrix
 */
-func NewSparseMatrix(nonzeros, rows, cols uint32) (matrix *SparseMatrix) {
+func NewSparseMatrix(nonzeros, rows, cols int) (matrix *SparseMatrix) {
 	matrix = &SparseMatrix{Nonzeros: nonzeros, Rows: rows, Cols: cols}
-	matrix.Ir = make([]uint32, nonzeros)
-	matrix.Jc = make([]uint32, cols+1)
+	matrix.Ir = make([]int, nonzeros)
+	matrix.Jc = make([]int, cols+1)
 	matrix.Sr = make([]float32, nonzeros)
 	return
 }
@@ -23,7 +30,7 @@ func NewSparseMatrix(nonzeros, rows, cols uint32) (matrix *SparseMatrix) {
 /*
 Set sets value v of  the [i,j] cell
 */
-func (m *SparseMatrix) Set(i, j uint32, v float32) {
+func (m *SparseMatrix) Set(i, j int, v float32) {
 	if m.index < m.Nonzeros {
 		m.Ir[m.index] = i
 		m.Jc[j]++
@@ -38,11 +45,131 @@ func (m *SparseMatrix) Set(i, j uint32, v float32) {
 Pack packs matrix
 */
 func (m *SparseMatrix) Pack() {
-	var j uint32
-	for ; j < m.Cols; j++ {
+	for j := 0; j < m.Cols; j++ {
 		m.Jc[j+1] += m.Jc[j]
 	}
 	m.Jc[0] = 0
+}
+
+/*
+AsyncronousBeliefPropagation - runs lda
+*/
+func (m *SparseMatrix) AsyncronousBeliefPropagation(ALPHA, BETA float32, J, NN int, startcond, OUTPUT bool, phi, theta, mu []float32) {
+	rand.Seed(time.Now().UnixNano())
+	var (
+		W                      = m.Rows
+		D                      = m.Cols
+		wi, di, j, topic, iter int
+		i                      int
+		xitot, mutot, xi       float32
+		perp                   float64
+	)
+	WBETA := float32(W) * BETA
+	JALPHA := float32(J) * ALPHA
+	phitot := make([]float32, J)
+	thetad := make([]float32, D)
+	jc := m.Jc
+	ir := m.Ir
+	sr := m.Sr
+	/* random initialization */
+	for di = 0; di < D; di++ { // go throught documents
+		for i = jc[di]; i < jc[di+1]; i++ { // go throught columns
+			wi = ir[i]
+			xi = sr[i]
+			thetad[di] += xi
+			xitot += xi
+			// pick a random topic 0..J-1
+			topic = rand.Intn(J)
+			mu[i*J+topic] = 1.0     // assign this word token to this topic
+			phi[wi*J+topic] += xi   // increment phi count matrix
+			theta[di*J+topic] += xi // increment theta count matrix
+			phitot[topic] += xi     // increment phitot matrix
+		}
+	}
+
+	for iter = 0; iter < NN; iter++ {
+		if OUTPUT {
+			if (iter%10) == 0 && (iter != 0) {
+				/* calculate perplexity */
+				perp = 0.0
+				for di = 0; di < D; di++ {
+					for i = jc[di]; i < jc[di+1]; i++ {
+						wi = ir[i]
+						xi = sr[i]
+						mutot = 0.0
+						for j = 0; j < J; j++ {
+							mutot += (phi[wi*J+j] + BETA) /
+								(phitot[j] + WBETA) *
+								(theta[di*J+j] + ALPHA) /
+								(thetad[di] + JALPHA)
+						}
+						perp -= math.Log(float64(mutot)) * float64(xi)
+					}
+				}
+				if iter%100 == 0 {
+					fmt.Printf("\tIteration %d of %d:\t%0.2f\n", iter, NN, math.Exp(perp/float64(xitot)))
+				}
+			}
+		}
+
+		var iJj int
+		for di = 0; di < D; di++ {
+			for i = jc[di]; i < jc[di+1]; i++ {
+				wi = ir[i]
+				xi = sr[i]
+				mutot = 0
+				for j = 0; j < J; j++ {
+					iJj = i*J + j
+					phi[wi*J+j] -= xi * mu[iJj]
+					phitot[j] -= xi * mu[iJj]
+					theta[di*J+j] -= xi * mu[iJj]
+					mu[iJj] = (phi[wi*J+j] + BETA) / (phitot[j] + WBETA) * (theta[di*J+j] + ALPHA)
+					mutot += mu[iJj]
+				}
+				for j = 0; j < J; j++ {
+					mu[i*J+j] /= mutot
+					phi[wi*J+j] += xi * mu[i*J+j]
+					phitot[j] += xi * mu[i*J+j]
+					theta[di*J+j] += xi * mu[i*J+j]
+				}
+			}
+		}
+	}
+}
+
+/*
+EvaluatePerplexity evaluates topic model quality in terms of perplexity
+*/
+func (m *SparseMatrix) EvaluatePerplexity(ALPHA, BETA, xitot float32, phi, theta, phitot, thetad []float32, drange, jrange []struct{}) (perp float64) {
+	/* calculate perplexity */
+	var (
+		W            = m.Rows
+		di, j, i     int
+		mutot        float32
+		thetap, phip []float32
+		J            = len(jrange)
+	)
+	WBETA := float32(W) * BETA
+	JALPHA := float32(J) * ALPHA
+	jc := m.Jc
+	ir := m.Ir
+	sr := m.Sr
+	for di = range drange {
+		thetap = theta[di*J : di*J+J]
+		for i = jc[di]; i < jc[di+1]; i++ {
+			mutot = 0.0
+			phip = phi[int(ir[i])*J : int(ir[i])*J+J]
+			for j = range jrange {
+				mutot += (phip[j] + BETA) /
+					(phitot[j] + WBETA) *
+					(thetap[j] + ALPHA) /
+					(thetad[di] + JALPHA)
+			}
+			perp -= math.Log(float64(mutot)) * float64(sr[i])
+		}
+	}
+	perp = math.Exp(perp / float64(xitot))
+	return
 }
 
 /*
